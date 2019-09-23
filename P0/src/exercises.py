a3f1699126735fe89c32e1d50e2f27e693bccdc9
antcc <antoniocoin07@gmail.com>
2019-09-23 11:53:15 +0200

    Archivos iniciales P0

---
+++ P0/src/exercises.py
@@ -0,0 +1,25 @@
+# Import the math module, so that we have access to the functions sqrt and pow
+from math import sqrt, pow
+
+def factorial(x):
+  """Return x!, assuming that x is a non-negative integer."""
+  pass
+
+def sumFile(filename):
+  """Each line of filename contains a float.  Return the sum of all lines in the
+  file."""
+  pass
+
+# This is the syntax for a Python class.
+class Point():
+  """Class that encapsulates a single point in the x-y plane."""
+
+  # This is the constructor for the class.  By convention, the first argument to
+  # any method of the class is self, referring to the variable itself.  This is
+  # similar to the "this" variable in other programming languages.
+
+  def __init__(self, x_coord, y_coord):
+    pass
+
+  def distanceFromOrigin(self):
+    return 0
